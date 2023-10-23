b29a3d9d12f92ad2b7b6218a2aa83652190ac719
yy97831 <yy318@outtlook.com>
2023-10-23 23:38:59 +0100

    updated code

--- bayesian_linear_regression_example.py
+++ bayesian_linear_regression_example.py
@@ -1,4 +1,3 @@
-# Import required libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
@@ -11,8 +10,10 @@ X = np.linspace(-1, 1, N)
 Y = true_w0 + true_w1 * X + np.random.normal(scale=0.2, size=N)
 
 # Initial prior
+alpha = 2.0
+beta = 10.0
 prior_mean = np.array([0.0, 0.0])
-prior_cov = np.array([[0.1, 0.0], [0.0, 0.1]])
+prior_cov = np.array([[alpha, 0.0], [0.0, alpha]])
 
 # Grid for w0 and w1
 w0_range = np.linspace(-1, 1, 100)
@@ -20,32 +21,19 @@ w1_range = np.linspace(-1, 1, 100)
 W0, W1 = np.meshgrid(w0_range, w1_range)
 pos = np.dstack((W0, W1))
 
-# Function to compute likelihood for a single data point
-def compute_likelihood(x, y, w0_range, w1_range, beta=10.0):
-    likelihood = np.ones((len(w0_range), len(w1_range)))
-    for i, w0 in enumerate(w0_range):
-        for j, w1 in enumerate(w1_range):
-            likelihood[i, j] = np.exp(-0.5 * beta * (y - (w0 + w1 * x)) ** 2)
-    return likelihood
+def update_posterior_analytically(x, y, prior_mean, prior_cov, beta=10.0):
+    X_matrix = np.column_stack([np.ones_like(x), x])
+    S_N_inv = np.linalg.inv(prior_cov) + beta * X_matrix.T @ X_matrix
+    S_N = np.linalg.inv(S_N_inv)
+    m_N = S_N @ (np.linalg.inv(prior_cov) @ prior_mean + beta * X_matrix.T @ y)
+    return m_N, S_N
 
-# Function to update posterior based on all observed data points
-def update_posterior(x, y, w0_range, w1_range, prior_mean, prior_cov, beta=10.0):
-    posterior = multivariate_normal.pdf(pos, mean=prior_mean, cov=prior_cov)
-    for xi, yi in zip(x, y):
-        likelihood = compute_likelihood(xi, yi, w0_range, w1_range, beta)
-        unnormalized_posterior = likelihood * posterior
-        posterior = unnormalized_posterior / np.sum(unnormalized_posterior)
-    return posterior
-
-# Explicitly specify the number of data points to observe at each stage
 stages = [[], [0], [0, 19], list(range(20))]
 
-# Start plotting
 fig, axs = plt.subplots(len(stages), 3, figsize=(15, 20))
 axs[0, 0].axis('off')
 
 for i, num_points in enumerate(stages):
-    # Add axis labels and titles
     axs[i, 0].set_xlabel('w0')
     axs[i, 0].set_ylabel('w1')
     axs[i, 1].set_xlabel('w0')
@@ -53,37 +41,32 @@ for i, num_points in enumerate(stages):
     axs[i, 2].set_xlabel('X')
     axs[i, 2].set_ylabel('Y')
     
+    x_observed = X[num_points]
+    y_observed = Y[num_points]
+    
     if not num_points:
-        # Prior only (no data observed yet)
         prior_values = multivariate_normal.pdf(pos, mean=prior_mean, cov=prior_cov)
         axs[i, 1].contourf(W0, W1, prior_values, levels=50, cmap='jet')
         axs[i, 1].set_title('Prior')
-        # Sample lines from the prior
         for _ in range(6):
             w_sample = np.random.multivariate_normal(prior_mean, prior_cov)
             axs[i, 2].plot(X, w_sample[0] + w_sample[1] * X, lw=1)
         axs[i, 2].set_title('Sample Lines from Prior')
     else:
-        # Data points observed so far
-        x_observed = X[num_points]
-        y_observed = Y[num_points]
-        # Plot observed data points
-        axs[i, 2].scatter(x_observed, y_observed, color='blue')
-        # Compute likelihood based on all observed data points
-        likelihood = compute_likelihood(x_observed[-1], y_observed[-1], w0_range, w1_range)
+        likelihood = np.exp(-0.5 * beta * np.sum((y_observed.reshape(-1, 1, 1) - W0 - W1 * x_observed.reshape(-1, 1, 1))**2, axis=0))
         axs[i, 0].contourf(W0, W1, likelihood, levels=50, cmap='jet')
         axs[i, 0].set_title(f'Likelihood (Data Points: {len(num_points)})')
-        # Update and plot posterior
-        posterior = update_posterior(x_observed, y_observed, w0_range, w1_range, prior_mean, prior_cov)
+        
+        post_mean, post_cov = update_posterior_analytically(x_observed, y_observed, prior_mean, prior_cov)
+        posterior = multivariate_normal.pdf(pos, mean=post_mean, cov=post_cov)
         axs[i, 1].contourf(W0, W1, posterior, levels=50, cmap='jet')
         axs[i, 1].set_title('Posterior')
-        # Sample lines from the posterior
+        
         for _ in range(6):
-            idx = np.unravel_index(np.random.choice(posterior.size, p=posterior.ravel()), posterior.shape)
-            w_sample = [w0_range[idx[0]], w1_range[idx[1]]]
+            w_sample = np.random.multivariate_normal(post_mean, post_cov)
             axs[i, 2].plot(X, w_sample[0] + w_sample[1] * X, lw=1)
+        axs[i, 2].scatter(x_observed, y_observed, color='blue')
         axs[i, 2].set_title('Sample Lines from Posterior')
 
-# Show the plots
 plt.tight_layout()
 plt.show()
